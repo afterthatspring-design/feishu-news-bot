bb418b3cfd9c7d90ecb6d66088e26e316d4e7362
afterthatspring-design <afterthatspring@gmail.com>
2026-04-03 14:42:43 +0800

    Update and rename main.py to index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ APP_SECRET = "XSHiz00HzeiN4xqFtjwVMbJ8EkNExlpp"
 CHAT_ID = "oc_bf1cf0b695aa6eccaac36b45538b6c99"
 
 def handler(request):
-    # 1️⃣ 获取 token
+    # 获取 token
     res = requests.post(
         "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal",
         json={
@@ -16,25 +16,16 @@ def handler(request):
     )
     token = res.json()["tenant_access_token"]
 
-    # 2️⃣ 构造消息
+    # 生成消息
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     text = f"📰 测试消息 {today}"
 
-    # 3️⃣ 发消息
-    r = requests.post(
+    # 发送消息
+    requests.post(
         "https://open.feishu.cn/open-apis/im/v1/messages",
-        headers={
-            "Authorization": f"Bearer {token}"
-        },
-        params={
-            "receive_id_type": "chat_id"
-        },
-        json={
-            "receive_id": CHAT_ID,
-            "msg_type": "text",
-            "content": f'{{"text":"{text}"}}'
-        }
+        headers={"Authorization": f"Bearer {token}"},
+        params={"receive_id_type": "chat_id"},
+        json={"receive_id": CHAT_ID, "msg_type": "text", "content": f'{{"text":"{text}"}}'}
     )
 
-    print(r.text)
     return {"statusCode": 200, "body": "ok"}
